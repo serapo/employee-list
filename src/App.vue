97c47f3da5fa29b605dc2135472ce96744153a7e
Serap <77828715+serapo@users.noreply.github.com>
2022-11-01 17:20:04 +0300

    employee

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <employe-list></employe-list>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeList from './components/EmployeList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color:darkblue ;
 }
 </style>
---
+++ src/components/EmployeList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+  <div class="container">
+      <h1>Employee List</h1><br>
+      <ul></ul>
+  <ul>
+    <li>dfkfklfd</li>
+    <li>ffdsfdfd</li>
+    <li>fgdfgd</li>
+  </ul>
+  <ul>
+    <li>dfkfklfd</li>
+    <li>ffdsfdfd</li>
+    <li>fgdfgd</li>
+  </ul>
+  <ul>
+    <li>dfkfklfd</li>
+    <li>ffdsfdfd</li>
+    <li>fgdfgd</li>
+  </ul>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'EmployeList',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  position: relative;
+  background-color: black;
+  width: 30%;
+  height: 90vh;
+  display: flex;
+  border-radius: 70px;
+  align-content: center;
+  justify-content: center;
+}
+div{
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+ul {
+  background-color: aqua;
+  width: 30%;
+  height: 50px;
+  display:grid;
+}
+</style>
